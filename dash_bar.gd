163a615ad305c237e5c8820dbd014b1a1cb76181
alex <myronrodriguez19@gmail.com>
2025-02-13 18:58:43 -0800

    Added Dash Bar

---
+++ dash_bar.gd
@@ -0,0 +1,26 @@
+extends ProgressBar
+
+@onready var dash_available: Timer = $"../Dash_Available"
+var dash_time_left = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void: 
+		if dash_available.time_left <= 0:
+			set_visible(false)
+			prog_reset()
+		else:
+			set_visible(true)
+			prog_down()
+		
+		print(dash_available.time_left)
+			
+func prog_down():
+	$".".value-=1
+
+func prog_reset():
+	$".".value = $".".max_value
