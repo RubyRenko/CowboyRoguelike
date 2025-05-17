8593753a563a991b1e1f5cc85c21dfd9aed0f284
LifeHckr <jarodthereal@gmail.com>
2025-05-16 20:10:17 -0700

    Implement elimination based waves

--- Rooms/Desert/desert_room.gd
+++ Rooms/Desert/desert_room.gd
@@ -36,13 +36,15 @@ var rng = RandomNumberGenerator.new()
 				load("res://Enemies/GoatHead/goat_head.tscn"), load("res://Enemies/GoatHead/goat_head_alt.tscn"), #desert enemies
 				load("res://Enemies/Moths/moth.tscn"), load("res://Enemies/Moths/moth_alt.tscn"), load("res://Enemies/Larva/larva.tscn") #forest enemies
 				]
-@onready var wave_timer = $Gui/WaveTimer
+@onready var wave_timer : Timer = $Gui/WaveTimer
 @onready var wave_display = $Gui/WaveAnim
 @onready var shop_spawn = load("res://Shop/shop.tscn")
 @onready var boss = load("res://Enemies/Chupacabra_New/chubacabra_new.tscn")
 @onready var wave_sfx = $WaveSfxPlayer
 @onready var nav_arrow = $CowboyPlayer/nav_arrow
 
+@export var enemies_rem_label : Label
+var enemies_rem_text : String = "Enemies Remaining: "
 
 #ids to get each tile from the tilemap
 var tile_ids = [Vector2i(0, 0), Vector2i(1,0), #base tiles
@@ -78,43 +80,7 @@ func _process(_delta):
 		player.die()
 		await get_tree().create_timer(2).timeout
 		get_tree().change_scene_to_file("res://GeneralUI/main_menu2.tscn")
-	
-	"""if Input.is_action_just_pressed("ui_accept"):
-		clean_up()
-		start_up()"""
 
-"""func create_room(width, height, padding = 6):
-	#nested for loops to get i rows and j columns
-	#padding is so that around the room there's still tiles and doesn't stop suddenly
-	for i in range(-padding, height+padding):
-		for j in range(-padding*2, width+(padding*2)):
-			#0 is the layer, Vector2i(j,i) is the coordinate the tile will be placed at
-			#0 is what tileset it is
-			#tileset_prob is the coordinates for the actual tile from the tileset
-			tilemap.set_cell(0, Vector2i(j,i), 0, tileset_prob.pick_random())
-			
-			if i == 15 || i == height - 15 || j == 15 || j == width - 15:
-				if i > 0 && i < height && j > 0 && j < width:
-					spawn_points.append(Vector2i(j, i))
-	
-	#creates invisible wall around the edge of the room
-	for i in range(height+1):
-		tilemap.set_cell(1, Vector2i(-1, i), 1, Vector2i(0,0))
-		tilemap.set_cell(1, Vector2i(width+1, i), 1, Vector2i(0,0))
-		if i % 2 == 0:
-			var cactus = tile_detail.tile_set.get_pattern(randi_range(0,1))
-			var cactus2 = tile_detail.tile_set.get_pattern(randi_range(0,1))
-			tile_detail.set_pattern(Vector2i(-1, i), cactus)
-			tile_detail.set_pattern(Vector2i(width+1, i), cactus2)
-		
-	for i in range(width+1):
-		tilemap.set_cell(1, Vector2i(i,-1), 1, Vector2i(0,0))
-		tilemap.set_cell(1, Vector2i(i, height+1), 1, Vector2i(0,0))
-		if i % 2 == 0:
-			var cactus = tile_detail.tile_set.get_pattern(randi_range(0,1))
-			var cactus2 = tile_detail.tile_set.get_pattern(randi_range(0,1))
-			tile_detail.set_pattern(Vector2i(i, -1), cactus)
-			tile_detail.set_pattern(Vector2i(i, height+1), cactus2)"""
 
 func create_room(width, height, padding = 12):
 	var terrain_start_point : Array
@@ -131,9 +97,6 @@ func create_room(width, height, padding = 12):
 			|| j == 15 && i >= 5|| j == width - 15 && i >= 5:
 				if i > 0 && i < height && j > 0 && j < width:
 					spawn_points.append(Vector2i(j, i))
-			
-			"if randi_range(0, 45) == 0:
-				terrain_start_point.append(Vector2i(j, i))"
 	
 	# sets the invisible walls
 	for i in (height+1):
@@ -206,95 +169,10 @@ func spawn_desert_pattern(pattern, spawn_point, x_dim, y_dim, invalid_array):
 		for x in x_dim:
 			for y in y_dim:
 				invalid_array.append(spawn_point + Vector2i(x, y))
-"""func create_detail(width, height):
-	var invalid_array = [
-		Vector2i(width/2-1, height/2-1), Vector2i(width/2, height/2-1), Vector2i(width/2+1, height/2-1),
-		Vector2i(width/2-1, height/2), Vector2i(width/2, height/2), Vector2i(width/2+1, height/2),
-		Vector2i(width/2-1, height/2+1),Vector2i(width/2, height/2+1), Vector2i(width/2+1, height/2+1)]
-		
-	#create buildings
-	for i in range(randi_range(10, 20)):
-		var spawn_x = randi_range(1, width-1)
-		var spawn_y = randi_range(1, height-1)
-		var spawning_tiles = [
-			Vector2i(spawn_x, spawn_y), Vector2i(spawn_x+1, spawn_y), Vector2i(spawn_x+2, spawn_y),
-			Vector2i(spawn_x, spawn_y+1), Vector2i(spawn_x+1, spawn_y+1), Vector2i(spawn_x+2, spawn_y+1),
-			Vector2i(spawn_x, spawn_y+2), Vector2i(spawn_x+1, spawn_y+2), Vector2i(spawn_x+2, spawn_y+2)
-		]
-		
-		var invalid_spawn = true
-		while invalid_spawn:
-			var found_invalid = false
-			for tile in spawning_tiles:
-				if tile in invalid_array:
-					spawn_x += randi_range(-3, 3)
-					spawn_y += randi_range(-3, 3)
-					found_invalid = true
-				elif tile_detail.get_cell_atlas_coords(tile) != Vector2i(-1, -1):
-					invalid_array.append(Vector2i(spawn_x, spawn_y))
-					spawn_x += randi_range(-3, 3)
-					spawn_y += randi_range(-3, 3)
-					found_invalid = true
-			if found_invalid:
-				spawning_tiles = [
-				Vector2i(spawn_x, spawn_y), Vector2i(spawn_x+1, spawn_y), Vector2i(spawn_x+2, spawn_y),
-				Vector2i(spawn_x, spawn_y+1), Vector2i(spawn_x+1, spawn_y+1), Vector2i(spawn_x+2, spawn_y+1),
-				Vector2i(spawn_x, spawn_y+2), Vector2i(spawn_x+1, spawn_y+2), Vector2i(spawn_x+2, spawn_y+2)
-				]
-			elif !found_invalid:
-				invalid_spawn = false
-		
-		spawning_tiles = [
-			Vector2i(spawn_x, spawn_y), Vector2i(spawn_x+1, spawn_y), Vector2i(spawn_x+2, spawn_y),
-			Vector2i(spawn_x, spawn_y+1), Vector2i(spawn_x+1, spawn_y+1), Vector2i(spawn_x+2, spawn_y+1),
-			Vector2i(spawn_x, spawn_y+2), Vector2i(spawn_x+1, spawn_y+2), Vector2i(spawn_x+2, spawn_y+2)
-		]
-		var pattern = tile_detail.tile_set.get_pattern(randi_range(5, 7))
-		for tile in spawning_tiles:
-			invalid_array.append(tile)
-			if tile in spawn_points:
-				spawn_points.remove_at(spawn_points.find(tile))
-		tile_detail.set_pattern(Vector2i(spawn_x, spawn_y), pattern)
-	
-	#create cactus
-	for i in range(randi_range(25, 50)):
-		var spawn_x = randi_range(1, width-1)
-		var spawn_y = randi_range(1, height-1)
-		var spawning_tiles = [
-			Vector2i(spawn_x, spawn_y), Vector2i(spawn_x, spawn_y +1)
-		]
-		
-		var invalid_spawn = true
-		while invalid_spawn:
-			var found_invalid = false
-			for tile in spawning_tiles:
-				if tile in invalid_array:
-					spawn_x += randi_range(-3, 3)
-					spawn_y += randi_range(-3, 3)
-					found_invalid = true
-				elif tile_detail.get_cell_atlas_coords(tile) != Vector2i(-1, -1):
-					invalid_array.append(Vector2i(spawn_x, spawn_y))
-					spawn_x += randi_range(-3, 3)
-					spawn_y += randi_range(-3, 3)
-					found_invalid = true
-			if found_invalid:
-				spawning_tiles = [
-				Vector2i(spawn_x, spawn_y), Vector2i(spawn_x, spawn_y+1)
-				]
-			elif !found_invalid:
-				invalid_spawn = false
-		
-		var pattern = tile_detail.tile_set.get_pattern(randi_range(0, 4))
-		for tile in spawning_tiles:
-			invalid_array.append(tile)
-			if tile in spawn_points:
-				spawn_points.remove_at(spawn_points.find(tile))
-		tile_detail.set_pattern(Vector2i(spawn_x, spawn_y), pattern)"""
 
 func spawn_enemy(spawn_pos, difficult = 1):
 	#create a new enemy instance and set the dddddddddposition
 	var e = enemies.pick_random().instantiate()
-	"e.hp += (e.hp/2) * (difficulty-1)"
 	e.position = spawn_pos
 	add_child(e)
 
@@ -315,21 +193,23 @@ func clean_up():
 		if child.is_in_group("enemy") || child.is_in_group("enemy_prod"):
 			child.queue_free()
 
+var enemies_spawned : int = 0
+var enemies_left : int = 0
 func spawn_wave(difficulty):
+	wave_timer.stop()
+	enemies_rem_label.visible = true
 	var spawn_array = spawn_points.duplicate()
 	var spawn_1 = spawn_array.slice(0, spawn_array.size()/4)
 	var spawn_2 = spawn_array.slice(spawn_array.size()/4, (spawn_array.size()/4)*2)
 	var spawn_3 = spawn_array.slice((spawn_array.size()/4)*2, (spawn_array.size()/4)*3)
 	var spawn_4 = spawn_array.slice((spawn_array.size()/4)*2, spawn_array.size())
 	var subsections = [spawn_1, spawn_2, spawn_3, spawn_4]
-	"print(spawn_array.size())
-	print(spawn_1)
-	print(spawn_2)
-	print(spawn_3)
-	print(spawn_4)"
 	
 	var player_position = tilemap.local_to_map(player.position)
 	var num_to_spawn = difficulty * 4 + randi_range(0, 3)
+	enemies_spawned = num_to_spawn
+	enemies_left = num_to_spawn
+	enemies_rem_label.text = enemies_rem_text + str(enemies_left)
 	for i in range(num_to_spawn):
 		var section = subsections.pick_random()
 		var spawn_pos = section.pick_random()
@@ -343,22 +223,6 @@ func spawn_wave(difficulty):
 func _on_wave_timer_timeout():
 	print("wave ", wave)
 	wave_display.display_wave(wave)
-	"""if wave == 1:
-		var chupacabra = boss.instantiate()
-		chupacabra.position = tilemap.map_to_local(Vector2i(room_width/2, room_height/2))
-		chupacabra.scale = Vector2(1.5, 1.5)
-		add_child(chupacabra)"""
-	"""if wave == 1:
-		#clears the previous waves and stops timer
-		wave_timer.stop()
-		clean_up()
-		#spawn shop
-		var shop = shop_spawn.instantiate()
-		shop.position = player.position
-		shop.add_to_group("shop")
-		add_child(shop)
-		print("shop spawn")
-		difficulty += 3"""
 	if wave == 16:
 		get_tree().change_scene_to_file("res://Rooms/Forest/forest_room.tscn")
 	elif wave == 15:
@@ -368,10 +232,9 @@ func _on_wave_timer_timeout():
 		chupacabra.position = tilemap.map_to_local(Vector2i(room_width/2, room_height/2))
 		chupacabra.scale = Vector2(1.5, 1.5)
 		add_child(chupacabra)
-		
+		enemies_rem_label.text = enemies_rem_text.to_upper() + "1"
 	elif wave % 5 == 0:
 		shop_wave()
-		
 	elif wave == 14:
 		wave_sfx.play_sfx("new_wave")
 		$Gui/WaveBarLabel.text = "BOSS INCOMING:"
@@ -391,6 +254,7 @@ func _on_wave_timer_timeout():
 	
 func shop_wave():
 	#clears the previous waves and stops timer
+	enemies_rem_label.visible = false
 	wave_sfx.play_sfx("shop_summon")
 	wave_timer.stop()
 	clean_up()
@@ -413,3 +277,9 @@ func _on_child_exiting_tree(node):
 	if node.is_in_group("enemy") && node.hit_by_player:
 		var kill_sounds = ["kill1", "kill2", "kill3", "kill4"]
 		wave_sfx.play_sfx(kill_sounds.pick_random())
+		enemies_left -= 1
+		enemies_rem_label.text = enemies_rem_text + str(enemies_left)
+		if enemies_left <= 0:
+			wave_timer.wait_time = 5
+			wave_timer.start()
+			
